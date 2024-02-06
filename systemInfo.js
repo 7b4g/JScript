b81a822a6f70d9961c6645977c59f79f31f9b077
7bugs <159054040+7bugs@users.noreply.github.com>
2024-02-07 01:11:28 +0400

    Update systemInfo.js

--- systemInfo.js
+++ systemInfo.js
@@ -19,97 +19,97 @@ var	NowTime,
 	Programs,
 	Printer;
 
-	//  date
-	var d = new Date, mon, day;
+//  date
+var d = new Date, mon, day;
 
-	mon = "" + (d.getMonth()+1);
-	day = "" + d.getDate();
-	if(mon.length == 1) {		
-		mon = "0" + mon;
-	}
-	if(day.length == 1) {
-		day = "0" + day;
-	}
-	NowTime =  d.getHours()+":"+d.getMinutes()+":"+d.getSeconds() + " " + day + "." + mon + "."+d.getFullYear();
-
-
-	// RAM
-	var	query = wmi.ExecQuery("Select * from Win32_PhysicalMemory");		
-	var accitem; 
-
-	MemoryBank = "";
-	for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
-		accitem = acc.item(); 
-		TotalRam += accitem.Capacity / 1024 / 1024 / 1024;	
-		MemoryBank += accitem.BankLabel + "[" + (accitem.Capacity / 1024/ 1024/ 1024) + "] ";
-	}
+mon = "" + (d.getMonth()+1);
+day = "" + d.getDate();
+if(mon.length == 1) {		
+	mon = "0" + mon;
+}
+if(day.length == 1) {
+	day = "0" + day;
+}
+NowTime =  d.getHours()+":"+d.getMinutes()+":"+d.getSeconds() + " " + day + "." + mon + "."+d.getFullYear();
 
 
-	// OS, FIO, PC name, user login 
-	query = wmi.ExecQuery("SELECT * FROM Win32_OperatingSystem");
+// RAM
+var	query = wmi.ExecQuery("Select * from Win32_PhysicalMemory");		
+var accitem; 
 
-	for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
-		accitem = acc.item();
-	}
-	OS = accitem.Caption;
-	FIO = accitem.Description;
-	Machine = accitem.CSName.split(" ", 2);
-	UserLogin = accitem.Username;
-	OSbit = accitem.OSArchitecture.split("-")[0];
-
-
-	// processor
-	query = wmi.ExecQuery("SELECT * FROM Win32_Processor"); 	
-
-	for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
-		accitem = acc.item();
-	}
-	Processor = accitem.Name;
+MemoryBank = "";
+for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
+	accitem = acc.item(); 
+	TotalRam += accitem.Capacity / 1024 / 1024 / 1024;	
+	MemoryBank += accitem.BankLabel + "[" + (accitem.Capacity / 1024/ 1024/ 1024) + "] ";
+}
 
 
-	// IP 
-	query = wmi.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = TRUE"); 	
+// OS, FIO, PC name, user login 
+query = wmi.ExecQuery("SELECT * FROM Win32_OperatingSystem");
 
-	NetCard = "";
-	for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
-		accitem = acc.item();
-		NetCard += "[" + accitem.IPAddress(0) + "]";
-	}
-
-
-	// hard drive
-	query = wmi.ExecQuery("SELECT * FROM Win32_LogicalDisk WHERE DriveType = 3 and Size != NULL"); 	
-
-	LocalDrives = "";
-	for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
-		accitem = acc.item();
-		LocalDrives += accitem.Caption;
-		var temp = "" + accitem.Size/1024/1024/1024;		
-		LocalDrives += "[" + temp.substr(0,temp.indexOf(".")+3) + "]"
-	}
+for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
+	accitem = acc.item();
+}
+OS = accitem.Caption;
+FIO = accitem.Description;
+Machine = accitem.CSName.split(" ", 2);
+UserLogin = accitem.Username;
+OSbit = accitem.OSArchitecture.split("-")[0];
 
 
-	// check installed programs
-	// change "Program1 name" to your value
-	query = wmi.ExecQuery("SELECT * FROM Win32_Product");
+// processor
+query = wmi.ExecQuery("SELECT * FROM Win32_Processor"); 	
 
-	Programs = "";
-	for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
-		accitem = acc.item();
-		if (accitem.Name == "Program1 name") {
-			Programs = "Program1";
-			break;
-		} else if (accitem.Name == "Program2 name") { 
-			Programs = "Program2";
-			break;
-		} else {Programs = "Programs not found"};
-	}
+for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
+	accitem = acc.item();
+}
+Processor = accitem.Name;
+
+
+// IP 
+query = wmi.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = TRUE"); 	
+
+NetCard = "";
+for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
+	accitem = acc.item();
+	NetCard += "[" + accitem.IPAddress(0) + "]";
+}
+
+
+// hard drive
+query = wmi.ExecQuery("SELECT * FROM Win32_LogicalDisk WHERE DriveType = 3 and Size != NULL"); 	
+
+LocalDrives = "";
+for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
+	accitem = acc.item();
+	LocalDrives += accitem.Caption;
+	var temp = "" + accitem.Size/1024/1024/1024;		
+	LocalDrives += "[" + temp.substr(0,temp.indexOf(".")+3) + "]"
+}
+
+
+// check installed programs
+// change "Program1 name" to your value
+query = wmi.ExecQuery("SELECT * FROM Win32_Product");
+
+Programs = "";
+for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
+	accitem = acc.item();
+	if (accitem.Name == "Program1 name") {
+		Programs = "Program1";
+		break;
+	} else if (accitem.Name == "Program2 name") { 
+		Programs = "Program2";
+		break;
+	} else {Programs = "Programs not found"};
+}
 
 	
-	// detect monitor model (in progress)
-	query = wmi.ExecQuery("SELECT * FROM Win32_")
+// detect monitor model (in progress)
+query = wmi.ExecQuery("SELECT * FROM Win32_")
 
-	var Mmodels = {
+var Mmodels = {
     "AAC" : "AcerView",
     "ACR" : "Acer",
     "AOC" : "AOC",
@@ -180,31 +180,31 @@ var	NowTime,
 
 
 
-	// printer + port
-	query = wmi.ExecQuery("SELECT * FROM Win32_Printer");
+// printer + port
+query = wmi.ExecQuery("SELECT * FROM Win32_Printer");
 
-	Printer = ""
-	for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
-		accitem = acc.item();
-		Printer = "[ " + accitem.Name + " -- " + accitem.Portname + " ]";
-	}
+Printer = ""
+for(var acc = new Enumerator(query); !acc.atEnd(); acc.moveNext()) {
+	accitem = acc.item();
+	Printer = "[ " + accitem.Name + " -- " + accitem.Portname + " ]";
+}
 
 
 // info output
 var	FSO = WScript.CreateObject("Scripting.FileSystemObject");
 var openText = FSO.OpenTextFile(logPath + UserLogin + "_" + Machine + ".LOG", 8, true, -1);
 
-	openText.WriteLine("Time: " 		+ NowTime 		+ "\n\n"+
+	openText.WriteLine(		"Time: " 		+ NowTime 		+ "\n\n"+
     					"FIO: " 		+ FIO 			+ "\n" +
-    					"PC name: "		+ Machine   	+ "\n" +
+    					"PC name: "		+ Machine   		+ "\n" +
     					"RAM: " 		+ TotalRam 		+ "\n" +
     					"OS: " 			+ OS 			+ "\n" + 
-						"bit: "			+ OSbit			+ "\n" +
-    					"Processor: "	+ Processor 	+ "\n" + 
-						"IP: "			+ NetCard		+ "\n" +
-						"Disks: " 		+ LocalDrives	+ "\n" +
-						"Progs: "		+ Programs		+ "\n" +
-						"Printer: " 	+ Printer 
+					"bit: "			+ OSbit			+ "\n" +
+    					"Processor: "		+ Processor 		+ "\n" + 
+					"IP: "			+ NetCard		+ "\n" +
+					"Disks: " 		+ LocalDrives		+ "\n" +
+					"Progs: "		+ Programs		+ "\n" +
+					"Printer: " 		+ Printer 
 			   );
 
 	openText.Close();
